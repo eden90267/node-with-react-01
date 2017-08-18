2d51c1d71bad8a72ad182d58122c87a34ed5d8f3
Eden Liu <eden90267@gmail.com>
2017-08-18 18:03:07 +0800

    Impl

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 
 import {todos} from './todoReducer';
+import {userInfo} from "./userInfo";
 
 const rootReducer = combineReducers({
   todos,
+  userInfo,
   routing: routerReducer,
 });
 
